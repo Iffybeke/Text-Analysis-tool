6c5fb639a5959064622342ec8493203b3499827d
EmekeChukwuka <chukzemeke9@gmail.com>
2026-01-15 17:06:13 +0100

    obtained username from terminal input

---
+++ analysis.py
@@ -0,0 +1,12 @@
+print("Welcome to Text-Analysis-tool. I will mine and analyze text data for insights.")
+
+#Get Username
+
+#Print Message prompting for username
+print("\nTo begin, please enter your username:")
+
+#Get user input for username
+username = input("Username: ")
+
+#Print personalized greeting message
+print(f"\nHello, {username}! Let's get started with your text analysis.")
