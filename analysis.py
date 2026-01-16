d0101e3c8d819cf7551e20daf6d7c2d40fee4953
EmekeChukwuka <chukzemeke9@gmail.com>
2026-01-16 10:29:11 +0100

    Converted workflows into functions

--- analysis.py
+++ analysis.py
@@ -1,12 +1,17 @@
-print("Welcome to Text-Analysis-tool. I will mine and analyze text data for insights.")
+# Welcome user
+def welcome_user():
+    print("Welcome to Text-Analysis-tool. I will mine and analyze text data for insights.")
 
 #Get Username
+def get_username():
+    #Print Message prompting for username
+    usernameFrominput= input(f"\nTo begin, please enter your username:\n")
+    return usernameFrominput
 
-#Print Message prompting for username
-print("\nTo begin, please enter your username:")
+# Greet user
+def greet_user(name):
+    print(f"\nHello, {name}! Let's get started with your text analysis.")
 
-#Get user input for username
-username = input("Username: ")
-
-#Print personalized greeting message
-print(f"\nHello, {username}! Let's get started with your text analysis.")
+welcome_user()
+username = get_username()
+greet_user(username)
